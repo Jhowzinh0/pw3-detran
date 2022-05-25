d338c975502d46d376547e57a27943525fc3fd22
NicolasNL <email@test.com>
2022-05-25 14:43:17 -0300

    add responseEntity

--- pw3-detran-main/src/main/java/br/com/levi/coimbra/pw3detran/controller/pessoa_controller.java
+++ pw3-detran-main/src/main/java/br/com/levi/coimbra/pw3detran/controller/pessoa_controller.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.levi.coimbra.pw3detran.entity.Pessoa;
 import br.com.levi.coimbra.pw3detran.repository.PessoaRepository;
+import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
 
 @RestController
 @RequestMapping("/pessoa")
@@ -29,9 +31,12 @@ public class pessoa_controller {
 	}
 
 	@GetMapping("/(id)")
-	public Pessoa BuscarPorId(@PathVariable Integer id) {
-		Pessoa tipo = new Pessoa();
-		tipo= repository.findById(id).get();
-		return tipo;
+	public ResponseEntity<Pessoa> buscarporId(@PathVariable Integer id) {
+		
+		java.util.Optional<Pessoa> tipo = repository.findById(id);
+			if (!tipo.isPresent()) {
+				ResponseEntity.noContent().build();
 	}
+		return ResponseEntity.ok(tipo.get());
+}
 }
